dfeaa50a1210f33bc4b611443fe7d79bd9b33456
sumiharuno <135489110+sumiharuno@users.noreply.github.com>
2023-06-24 04:09:27 +0000

    [finish]4-3

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_20_110549) do
+ActiveRecord::Schema.define(version: 2023_06_23_120822) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
