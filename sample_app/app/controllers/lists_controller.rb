390bf98d4ee028b81dcb71b596b8a4a2ba44f6ac
sumiharuno <135489110+sumiharuno@users.noreply.github.com>
2023-06-24 08:38:24 +0000

    [finish]4-5

--- sample_app/app/controllers/lists_controller.rb
+++ sample_app/app/controllers/lists_controller.rb
@@ -9,7 +9,7 @@ class ListsController < ApplicationController
 
     list.save
 
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   
   end
 
@@ -18,14 +18,23 @@ class ListsController < ApplicationController
   end
 
   def show
+   @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
   end
 
   private
 
   def list_params
    params.require(:list).permit(:title, :body)
+
   end
 end
